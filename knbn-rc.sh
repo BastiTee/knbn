81211ce721bee71ba254f293c617d2cc30c043c0
Basti Tee <basti.tee@icloud.com>
2026-08-24 08:18:21 +0200

    Add fast prompting

--- knbn-rc.sh
+++ knbn-rc.sh
@@ -18,7 +18,9 @@ here="$( cd "$( dirname "$0" )"; pwd )"
 
 # Alias to invoke knbn by calling 'k'
 alias k="cd ${here} && uv run knbn"
-# Alias to create a new task by calling 't'
+# Alias to create a new task by prompting properties
 alias t="k add"
+# Alias to create a new task by title only (using all defaults)
+alias tt="k add --fast"
 # Developer shortcut to start knbn with demo data
 alias knbn-demo="cd ${here} && KNBN_DATA_DIR=demo uv run knbn"
